9d11c88e65b47e1037288b7346e9991c2b0694c5
shaunhossain <shaunhossain95@gmail.com>
2020-03-29 11:52:41 +0600

    sotoder bangla bornomala sikha

---
+++ app/src/main/java/com/shaunhossain/bornomala/SelectionTypeActivity.kt
@@ -0,0 +1,12 @@
+package com.shaunhossain.bornomala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectionTypeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selection_type)
+    }
+}
