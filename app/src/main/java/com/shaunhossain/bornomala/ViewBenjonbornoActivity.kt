e45961c8cd3235fad21cc5f9ac007ca1a88ee462
shaunhossain <shaunhossain95@gmail.com>
2020-03-29 11:57:49 +0600

    sotoder bangla bornomala sikha

---
+++ app/src/main/java/com/shaunhossain/bornomala/ViewBenjonbornoActivity.kt
@@ -0,0 +1,12 @@
+package com.shaunhossain.bornomala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewBenjonbornoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_benjonborno)
+    }
+}
---
+++ app/src/main/java/com/shaunhossain/bornomala/ViewSorbornoActivity.kt
@@ -0,0 +1,12 @@
+package com.shaunhossain.bornomala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewSorbornoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_sorborno)
+    }
+}
